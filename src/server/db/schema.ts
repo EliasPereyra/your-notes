ec224b73fd16aa388ee04d0710cc7e804cb4035f
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2024-04-30 10:37:13 -0300

    Create 3 more tables

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,7 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { sql } from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import { index, int, sqliteTableCreator, text } from "drizzle-orm/sqlite-core";
 
 /**
@@ -28,15 +28,44 @@ export const notes = createTable(
   }),
 );
 
-export const topic = createTable("topic", {
+export const topics = createTable("topic", {
   id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
   title: text("title", { length: 40 }),
   // add the user and notes relations
 });
 
-export author = createTable("author", { 
-  id: int("id", { mode: "number"}).primaryKey({ autoIncrement: true}),
-  name: text("name", { length: 40}).notNull(),
-  email: text("name", { length: 40, mode: "text"}).notNull(),
+export const authors = createTable("author", {
+  id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  name: text("name", { length: 40 }).notNull(),
+  email: text("name", { length: 40, mode: "text" }).notNull(),
   image: text("name"),
-})
+});
+
+const accounts = createTable("account", {
+  id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  userId: text("userId"),
+  type: text("type"),
+  provider: text(""),
+  providerAccountId: text("providerAccountId"),
+  refresh_token: text("refreshToken"),
+  refresh_token_expires_in: int("refreshTokenExpiresIn"),
+  access_token: text("accessToken"),
+  expires_at: int("expiresAt", { mode: "timestamp" }),
+  token_type: text("tokenType"),
+  scope: text("scope"),
+  id_token: text("idToken"),
+  session_state: text("sessionState"),
+});
+
+const session = createTable("session", {
+  id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  sessionToken: text("sessionToken"),
+  userId: text("userId"),
+  expires: int("expires", { mode: "timestamp" }),
+});
+
+const verificationToken = createTable("verificationToken", {
+  id: text("id"),
+  token: text("token").unique(),
+  expires: int("expires", { mode: "timestamp" }),
+});
